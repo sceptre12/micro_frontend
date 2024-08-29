dc051a8ee266e7ff54e0640718e4312880b1ce4e
Xavier Thomas <x.devr.space@gmail.com>
2024-08-28 21:44:01 -0400

    testing out exporting a remote and using it in a ssr environment

--- csr_login/modern.config.ts
+++ csr_login/modern.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
       appendPlugins([
         new ModuleFederationPlugin({
           name: 'login',
+          filename: 'remoteEntry.js',
           exposes: {
             '.': './src/exposedEntry.tsx',
             './button': './src/components/Button.tsx',
--- ssr_home_page/src/routes/page.tsx
+++ ssr_home_page/src/routes/page.tsx
@@ -1,102 +1,131 @@
 import { lazy, Suspense } from 'react';
 import { Helmet } from '@modern-js/runtime/head';
+import {
+  init,
+  registerRemotes,
+  loadRemote,
+} from '@module-federation/enhanced/runtime';
 import './index.css';
 
 // const LoginApp = lazy(() => import('login'));
-const ButtonComponent = lazy(() => import('login/button'));
 
-const Index = () => (
-  <div className="container-box">
-    <Helmet>
-      <link
-        rel="icon"
-        type="image/x-icon"
-        href="https://lf3-static.bytednsdoc.com/obj/eden-cn/uhbfnupenuhf/favicon.ico"
-      />
-    </Helmet>
-    <main>
-      <div className="title">
-        Welcome to
-        <img
-          className="logo"
-          src="https://lf3-static.bytednsdoc.com/obj/eden-cn/zq-uylkvT/ljhwZthlaukjlkulzlp/modern-js-logo.svg"
-          alt="Modern.js Logo"
+init({
+  name: 'home',
+  remotes: [
+    {
+      name: 'login',
+      entry: 'login@http://localhost:3002/mf-manifest.json',
+    },
+  ],
+});
+
+const Index = () => {
+  registerRemotes(
+    [
+      {
+        name: 'login',
+        entry: 'login@http://localhost:3002/mf-manifest.json',
+      },
+    ],
+    { force: true },
+  );
+
+  const ButtonComponent = lazy(() => loadRemote('login/button'));
+
+  return (
+    <div className="container-box">
+      <Helmet>
+        <link
+          rel="icon"
+          type="image/x-icon"
+          href="https://lf3-static.bytednsdoc.com/obj/eden-cn/uhbfnupenuhf/favicon.ico"
         />
-        <p className="name">Modern.js</p>
-        <Suspense fallback={<div>Loading button</div>}>
-          <ButtonComponent />
-        </Suspense>
-      </div>
-      <p className="description">
-        Get started by editing <code className="code">src/routes/page.tsx</code>
-      </p>
-      <div className="grid">
-        <a
-          href="https://modernjs.dev/guides/get-started/introduction.html"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="card"
-        >
-          <h2>
-            Guide
-            <img
-              className="arrow-right"
-              src="https://lf3-static.bytednsdoc.com/obj/eden-cn/zq-uylkvT/ljhwZthlaukjlkulzlp/arrow-right.svg"
-            />
-          </h2>
-          <p>Follow the guides to use all features of Modern.js.</p>
-        </a>
-        <a
-          href="https://modernjs.dev/tutorials/foundations/introduction.html"
-          target="_blank"
-          className="card"
-          rel="noreferrer"
-        >
-          <h2>
-            Tutorials
-            <img
-              className="arrow-right"
-              src="https://lf3-static.bytednsdoc.com/obj/eden-cn/zq-uylkvT/ljhwZthlaukjlkulzlp/arrow-right.svg"
-            />
-          </h2>
-          <p>Learn to use Modern.js to create your first application.</p>
-        </a>
-        <a
-          href="https://modernjs.dev/configure/app/usage.html"
-          target="_blank"
-          className="card"
-          rel="noreferrer"
-        >
-          <h2>
-            Config
-            <img
-              className="arrow-right"
-              src="https://lf3-static.bytednsdoc.com/obj/eden-cn/zq-uylkvT/ljhwZthlaukjlkulzlp/arrow-right.svg"
-            />
-          </h2>
-          <p>Find all configuration options provided by Modern.js.</p>
-        </a>
-        <a
-          href="https://github.com/web-infra-dev/modern.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="card"
-        >
-          <h2>
-            Github
-            <img
-              className="arrow-right"
-              src="https://lf3-static.bytednsdoc.com/obj/eden-cn/zq-uylkvT/ljhwZthlaukjlkulzlp/arrow-right.svg"
-            />
-          </h2>
-          <p>View the source code of Github, feel free to contribute.</p>
-        </a>
-      </div>
-    </main>
-    {/* <Suspense fallback={<div>Loading LoginAPp</div>}>
+      </Helmet>
+      <main>
+        <div className="title">
+          Welcome to
+          <img
+            className="logo"
+            src="https://lf3-static.bytednsdoc.com/obj/eden-cn/zq-uylkvT/ljhwZthlaukjlkulzlp/modern-js-logo.svg"
+            alt="Modern.js Logo"
+          />
+          <p className="name">Modern.js</p>
+          <Suspense fallback={<div>Loading button</div>}>
+            <ButtonComponent />
+          </Suspense>
+        </div>
+        <p className="description">
+          Get started by editing{' '}
+          <code className="code">src/routes/page.tsx</code>
+        </p>
+        <div className="grid">
+          <a
+            href="https://modernjs.dev/guides/get-started/introduction.html"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="card"
+          >
+            <h2>
+              Guide
+              <img
+                className="arrow-right"
+                src="https://lf3-static.bytednsdoc.com/obj/eden-cn/zq-uylkvT/ljhwZthlaukjlkulzlp/arrow-right.svg"
+              />
+            </h2>
+            <p>Follow the guides to use all features of Modern.js.</p>
+          </a>
+          <a
+            href="https://modernjs.dev/tutorials/foundations/introduction.html"
+            target="_blank"
+            className="card"
+            rel="noreferrer"
+          >
+            <h2>
+              Tutorials
+              <img
+                className="arrow-right"
+                src="https://lf3-static.bytednsdoc.com/obj/eden-cn/zq-uylkvT/ljhwZthlaukjlkulzlp/arrow-right.svg"
+              />
+            </h2>
+            <p>Learn to use Modern.js to create your first application.</p>
+          </a>
+          <a
+            href="https://modernjs.dev/configure/app/usage.html"
+            target="_blank"
+            className="card"
+            rel="noreferrer"
+          >
+            <h2>
+              Config
+              <img
+                className="arrow-right"
+                src="https://lf3-static.bytednsdoc.com/obj/eden-cn/zq-uylkvT/ljhwZthlaukjlkulzlp/arrow-right.svg"
+              />
+            </h2>
+            <p>Find all configuration options provided by Modern.js.</p>
+          </a>
+          <a
+            href="https://github.com/web-infra-dev/modern.js"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="card"
+          >
+            <h2>
+              Github
+              <img
+                className="arrow-right"
+                src="https://lf3-static.bytednsdoc.com/obj/eden-cn/zq-uylkvT/ljhwZthlaukjlkulzlp/arrow-right.svg"
+              />
+            </h2>
+            <p>View the source code of Github, feel free to contribute.</p>
+          </a>
+        </div>
+      </main>
+      {/* <Suspense fallback={<div>Loading LoginAPp</div>}>
       <LoginApp />
     </Suspense> */}
-  </div>
-);
+    </div>
+  );
+};
 
 export default Index;
